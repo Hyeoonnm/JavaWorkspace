587e03d170a87c58870df100774c3e6261298330
hyeoonnm <rkdwogus77@gmail.com>
2023-02-02 16:46:25 +0900

    java 공부

---
+++ src/Quiz/_Quiz_01.java
@@ -0,0 +1,13 @@
+package Quiz;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busName = "상암 08번 버스";
+        int min = 3;
+        float distance = 1.2f;
+
+        System.out.println(busName);
+        System.out.println("약 " + min + "분 후 도착");
+        System.out.println("남은 거리 " + distance + "km");
+    }
+}
---
+++ src/Quiz/_Quiz_02.java
@@ -0,0 +1,13 @@
+package Quiz;
+
+public class _Quiz_02 {
+    public static void main(String[] args) {
+
+        int height = 121;
+
+        String result = (height >= 120) ? "키가 " + height + "이므로 탑승이 가능합니다." : "키가 " + height + "이므로 탑승이 불가능합니다.";
+        System.out.println(result);
+
+
+    }
+}
